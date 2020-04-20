7db63d6deaddbae6f059d696705cc200eaabaab9
muondu <munenemuondu@gmail.com>
2020-04-20 12:27:25 +0300

    This is a python test for begginers which has only five questions

---
+++ README.md
@@ -0,0 +1,3 @@
+# My project
+This is a test. It has 5 questions of python the computer will ask the user to answer them. If the user fails one, it tell them they are wrong and the explanation.
+If they are write, it will tell them correct.
---
+++ test.py
@@ -0,0 +1,64 @@
+class Test:
+	print("Hello, Today we are going to have a test. It will be from 1 - 5")
+	
+	a = input("""
+	Question 1.
+	Are string mutuble?
+	True or
+	False
+	
+	""")
+	if a == "True":
+		print("Correct")
+	else:
+		print("You got it wrong, strings are mutuble")
+	
+	b = input("""
+	Question 2.
+	Can you make a number a string?
+	True or
+	False
+	
+	""")
+	if b == "True":
+		print("Correct")
+	else:
+		print("You got it wrong, you can make numbers strings by adding("" or '')")
+		
+		
+	c = input("""
+	Question 3.
+	Are tuples mutuble?
+	True or
+	False
+	
+	""")
+	if c == "True":
+		print("You got it wrong. Tuples are not mutuble")
+	else:
+		print("You got it correct, well done")
+		
+	d = input("""
+	Question 4.
+	Are dictionaries mutuble?
+	True or
+	False
+	
+	""")
+	if d == "False":
+		print("Correct")
+	else:
+		print("You got it wrong, dictionaries are not mutuble")
+		
+	e = input("""
+	Question 5.
+	Is there something called boolean in python?
+	True or
+	No
+	
+	""")
+	if e == "True":
+		print("Correct")
+	else:
+		print("You got it wrong, there is something like boolean.")
+
